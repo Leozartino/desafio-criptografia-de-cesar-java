7b75f292c993a1ab6b2971a572101a7fc346292f
Leozartino <leozartino@gmail.com>
2020-06-22 18:07:07 -0300

    feat: desafio completo ✅

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -1,34 +1,50 @@
 package challenge;
 
 public class CriptografiaCesariana implements Criptografia {
-    static int salto = 3;
-    static int valorIntervaloA;
-    static int valorIntervaloB;
-    static int valorIntervaloC;
-    static int valorIntervaloD;
-    static int valorFormula;
+    private final int SALTO = 3;
+    private int valorIntervaloA;
+    private int valorIntervaloB;
+    private int valorIntervaloC;
+    private int valorIntervaloD;
+    private int valorFormula;
 
     @Override
     public String criptografar(String texto) {
 
-        return algoritmoCesar(texto, salto);
+        return algoritmoCesar(texto, SALTO);
 
     }
 
     @Override
     public String descriptografar(String texto) {
 
-        return algoritmoCesar(texto, -salto);
+        return algoritmoCesar(texto, -SALTO);
 
     }
 
-    private String algoritmoCesar(String texto, int salto) {
+    private String algoritmoCesar(String texto, int SALTO) {
         validaTexto(texto);
+        verificaSalto(SALTO);
 
         char[] textoParaChar = texto.toLowerCase().toCharArray();
         int tamanhoTexto = textoParaChar.length;
         char[] textoCharMensagem = new char[tamanhoTexto];
 
+        for (int i = 0; i < tamanhoTexto; i++) {
+            if (textoParaChar[i] >= valorIntervaloA && textoParaChar[i] <= valorIntervaloB) {
+                textoCharMensagem[i] = (char) (textoParaChar[i] + SALTO);
+            } else if (textoParaChar[i] >= valorIntervaloC && textoParaChar[i] < valorIntervaloD) {
+                textoCharMensagem[i] = (char) (textoParaChar[i] + valorFormula);
+            } else {
+                textoCharMensagem[i] = textoParaChar[i];
+            }
+        }
+
+        String textoResultante = new String(textoCharMensagem);
+        return textoResultante;
+    }
+
+    private void verificaSalto(int salto) {
         if (salto < 0) {
             valorIntervaloA = 100;
             valorIntervaloB = 122;
@@ -42,26 +58,14 @@ public class CriptografiaCesariana implements Criptografia {
             valorIntervaloD = 122;
             valorFormula = -23;
         }
-
-        for (int i = 0; i < tamanhoTexto; i++) {
-            if (textoParaChar[i] >= valorIntervaloA && textoParaChar[i] <= valorIntervaloB) {
-                textoCharMensagem[i] = (char) (textoParaChar[i] + salto);
-            } else if (textoParaChar[i] >= valorIntervaloC && textoParaChar[i] < valorIntervaloD) {
-                textoCharMensagem[i] = (char) (textoParaChar[i] + valorFormula);
-            } else {
-                textoCharMensagem[i] = textoParaChar[i];
-            }
-        }
-
-        String textoResultante = new String(textoCharMensagem);
-        return textoResultante;
     }
 
     private void validaTexto(String texto) {
-        if (texto.isEmpty()) {
-            throw new IllegalArgumentException();
-        } else if (texto.equals(null)) {
-            throw new NullPointerException();
+        if (texto == null) {
+            throw new NullPointerException("O texto não pode ser nulo!");
+        }
+        if (texto.equals("")) {
+            throw new IllegalArgumentException("O texto esta vazio!");
         }
     }
 }
